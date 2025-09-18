78916937fd47072224dec738150ca8ec1eef8c91
ilyass mar <ilyass.marghine22@gmail.com>
2025-09-18 08:29:31 +0200

    added the classes of general user and client still not completed

---
+++ src/model/Client.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.HashMap;
+
+public class Client extends Personne {
+    private String idClient;
+    private HashMap<String, Compte> comptes;
+
+    // Constructor
+    public Client(String nom, String prenom, String email, String motDePasse, String idClient) {
+        super(nom, prenom, email, motDePasse);
+        this.idClient = idClient;
+        this.comptes = new HashMap<>();
+    }
+
+    // Getters and setters
+    public String getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(String idClient) {
+        this.idClient = idClient;
+    }
+
+    public HashMap<String, Compte> getComptes() {
+        return comptes;
+    }
+
+    // Methods to manage accounts
+    public void ajouterCompte(Compte compte) {
+        comptes.put(compte.getNumeroCompte(), compte);
+    }
+
+    public void supprimerCompte(String numeroCompte) {
+        comptes.remove(numeroCompte);
+    }
+
+    public Compte getCompte(String numeroCompte) {
+        return comptes.get(numeroCompte);
+    }
+}
